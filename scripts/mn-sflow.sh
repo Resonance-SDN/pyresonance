db8b1f3fde90705e5452d2d66bae7050900415bb
Software Defined Exchange <sdx@wollaston.noise.gatech.edu>
2013-09-22 12:52:52 -0400

    added ovs scripts for enabling sflow with openflow switches/mininet

---
+++ scripts/mn-sflow.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+################################################################################
+# Resonance Project                                                            #
+# Resonance implemented with Pyretic platform                                  #
+# author: Muhammad Shahbaz (muhammad.shahbaz@gatech.edu)                       #
+################################################################################
+
+case "$1" in
+   enable)
+      echo "Enabling sFlow ..." 
+      ovs-vsctl -- --id=@sflow create sflow agent=eth3 target=\"127.0.0.1:6343\" \
+      sampling=2 polling=20 -- -- set bridge $2 sflow=@sflow
+   ;;
+   disable)
+      echo "Disabling sFlow ..."  	
+	  ovs-vsctl -- clear bridge $2 sflow
+   ;;
+   *)
+      echo "Usage: mn-sflow.sh {enable|disable} {switch-id}"
+   ;;
+esac
