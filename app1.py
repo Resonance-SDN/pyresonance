a7cdaf50575889bd47f09b50ccc32d4a0a2addd3
Joshua Reich <jreich@cs.princeton.edu>
2014-01-16 18:24:36 -0500

    some more progress

--- app1.py
+++ app1.py
@@ -1,25 +1,32 @@
 
+from pyretic.lib.corelib import *
+from pyretic.lib.std import *
+
 from pyretic.pyresonance.base import FSMPolicy
 from pyretic.pyresonance.drivers.json_event import JSONEvent 
 
 def main():
 
-    def block_next(state):
+    def policy_next(state):
         if state['infected']:
-            return True
+            return drop
         else:
-            return False
+            return identity
 
     def infected_next(event):
         return event
 
-    fsm_description = { 'block' : (bool,False,block_next),
-                        'infected' : (bool,False,infected_next)   }
+    fsm_description = { 'policy' : ([drop,identity],
+                                    identity,
+                                    policy_next),
+                        'infected' : (bool,
+                                      False,
+                                      infected_next)   }
 
 
     fsm_pol = FSMPolicy(fsm_description)
-    json_event = JSONEvent(fsm_pol.event_msg_handler)            
 
+    json_event = JSONEvent(fsm_pol.event_msg_handler)            
 
 
 if __name__ == '__main__':
--- base.py
+++ base.py
@@ -6,25 +6,54 @@
 # author: Muhammad Shahbaz (muhammad.shahbaz@gatech.edu)                       #
 ################################################################################
 
+import ast
+
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 
-class FlowFSM(dict):
+class FlowFSM(DynamicPolicy):
     def __init__(self, fsm_description):
         self.type = dict()
         self.state = dict()
         self.next = dict()
-        for state_name,state_tuple in fsm_description.items():
+        for var_name,state_tuple in fsm_description.items():
             state_type, init_val, nextfn = state_tuple
-            self.type[state_name] = state_type
-            self.state[state_name] = init_val
-            self.next[state_name] = nextfn
+            self.type[var_name] = state_type
+            self.state[var_name] = init_val
+            self.next[var_name] = nextfn
+        super(FlowFSM,self).__init__(self.state['policy'])
 
-    def handle_event(self,event_name,event_value):
-        var_type = self.type[event_name]
-        val = var_type(event_value)
-        self.state[event_name] = self.next[event_name](val)
-                 
+    def handle_event(self,event_name,event_val_str):
+        var_name = event_name
+        var_type = self.type[var_name]
+        if var_type == bool:
+            event_val = ast.literal_eval(event_val_str)
+        else:
+            raise RuntimeError('not yet implemented')
+        print self.state
+        print '-------------'
+        next_val = self.next[var_name](event_val)
+        if next_val != self.state[var_name]:
+            self.state[var_name] = next_val
+            self.handle_var_change(var_name)
+        print self.state
+        print '============='
+        
+    def get_dependent_vars(self,var_name):
+        if var_name == 'infected':
+            return { 'policy' }
+        else:
+            return set()
+         
+    def handle_var_change(self,init_var_name):
+        dependent_vars = self.get_dependent_vars(init_var_name)
+        while len(dependent_vars) > 0:
+            var_name = dependent_vars.pop()
+            next_val = self.next[var_name](self.state)
+            if next_val != self.state[var_name]:
+                self.state[var_name] = next_val
+                dependent_vars |= self.get_dependent_vars(var_name)
+
     def current_state_string(self):
         return '{' + '\n'.join([str(name) + ' : ' + str(val) for name,val in self.state.items()]) + '}'
 
