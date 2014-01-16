57e4f2b93df9d61f2cf4aac03674007f1e87754a
Joshua Reich <jreich@cs.princeton.edu>
2014-01-16 15:47:03 -0500

    more progress

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
 
-from pyretic.pyresonance.base import FlowFSM
+from pyretic.pyresonance.base import FSMPolicy
 from pyretic.pyresonance.drivers.json_event import JSONEvent 
 
 def main():
@@ -16,23 +16,10 @@ def main():
     fsm_description = { 'block' : (bool,False,block_next),
                         'infected' : (bool,False,infected_next)   }
 
-    def event_msg_handler(event_msg):
-        event_name = event_msg['name']
-        event_value = event_msg['value']
-        event_flow = event_msg['flow']
 
-        print event_msg        
-        t,v,f = fsm_description[event_name]
-        print f(event_value)
+    fsm_pol = FSMPolicy(fsm_description)
+    json_event = JSONEvent(fsm_pol.event_msg_handler)            
 
-    json_event = JSONEvent(event_msg_handler)            
-
-    flow_fsm = FlowFSM(fsm_description)
-
-    print 'hello'
-    print flow_fsm.current_state_string()
-
-    return
 
 
 if __name__ == '__main__':
